6407adba08151b91d3ccbb45e6578b1bd8cd1cd8
viraj0808 <122769838+viraj0808@users.noreply.github.com>
2023-04-11 13:14:52 +0530

    Delete 8.3.py

--- python-Practice/8.3.py
+++
@@ -1,8 +0,0 @@
-def make_shirt(size="L",massage="yor r good"):
-    print("This is your shirt size :" + size +" and massage will be: "+massage )
-# using positional argument
-make_shirt("M","viraj")
-#using keyword argument
-make_shirt(size="XL",massage= "nice")    
-# default function
-make_shirt('size','massage')
